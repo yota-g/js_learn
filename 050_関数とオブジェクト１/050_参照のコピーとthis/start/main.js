f86686063faefcb204b813c1b55a8e5a1ec47a61
yota-g <yota18493@gmail.com>
2021-06-06 22:59:57 +0900

    section 7 start

--- 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -1,7 +1,43 @@
+//参照がコピーされた時のthisの挙動をみる。
+window.name = 'John'
 const person = {
     name: 'Tom',
     hello: function() {
         console.log('Hello ' + this.name);
+        a(); // hello Johnが呼び出される。これは単に関数の中でthisを呼んでいるだけなのでwindowオブジェクトを参照することになる.
+        const person = {
+            name : 'Tim', //ここが未定義だとthis.nameがundefinedになる。
+            hello: function () {
+                console.log( 'goodevening' + this.name) //このthisは1つ上の階層のpersonのname Timを参照する。
+                a();
+            }
+        }　　
+        person.hello(); //オブジェクトから呼び出したときは呼び出し元のオブジェクトから引用することができる。
     }
 }
+const ref = person.hello;//helloを()で実行するのではなく他の変数に代入する。
+//そしてその変数を実行する。
+ref();
+//Helloと表示される。
+//this.nameがあるのでpersonのnameの’Tom’を参照するのではないかと思うが、
+//window.nameとして値を入れているとrefを実行したときにHello Johnとなる。
+//これがmethodを他の変数にコピーした挙動となる。　
+
 person.hello();
+
+//person.helloをrefにコピーするとhelloというプロパティの参照している関数がコピーされる。
+//そのコピーされた関数に対して、refが参照する事になる。　
+//関数はオブジェクトの一種なので、関数の実態への参照がコピーされている。
+//refから関数を呼び出すとpersonオブジェクトを参照せずに直接関数を呼び出すことになる。
+//そうすると呼び出し元のオブジェクトがpersonではなくなるので、this.nameでTomが取れなくなる。
+
+//では、なぜ、window.nameを設定すると値をとってくることができるのか。
+//オブジェクトのメソッドして実行される場合は、thisで呼び出し元オブジェクトを参照。
+//関数として実行されるときは、thisはグローバルオブジェクトを参照するものとなる。
+//コンストラクター関数とクラスを使った時に例外があるが、基本的にはthisの参照先は上記のようになる。
+
+function a () {
+    console.log( 'Hello' + this.name)
+}
+
+//thisの値はsourcesのscopeのlocalに表示される。
--- 050_関数とオブジェクト１/060_コールバック関数とthis/start/main.js
+++ 050_関数とオブジェクト１/060_コールバック関数とthis/start/main.js
@@ -1,3 +1,5 @@
+//コールバック関数とthisに関して、
+
 window.name = 'John';
 
 const person = {
@@ -7,3 +9,20 @@ const person = {
     }
 }
 person.hello();
+//person.helloを他の関数の引数に与えた場合はどうなるのか。
+function fn(ref) {
+    ref();
+}
+
+fn(person.hello);
+// 実行結果がhello Johnとなるwindow.nameが参照されている。
+//これはメソッドを他の変数に代入していることと同じなので、let ref = person.hello のようなもの。　
+ //refが参照している関数が実行されることになる。
+//なのでfnの中に入れられたperson.helloはfunctionの機能のみを参照していることになる
+//そのため実行すると関数が実行され、関数の実行ではwindowオブジェクトにthisが格納されていることとなる。
+//refにperson.helloの関数がコピーされたものを参照させている。
+//refを実行するのでrefは直接関数を実行するのでthisはpersonではなくwindowオブジェクトになる。
+
+//オブジェクトのメソッドで実行される場合は、thisは呼び出し元のオブジェクト。
+//関数として実行されるときはthisはグローバルオブジェクトをとる。
+//引数で渡した段階でそれはすでに関数となるのでグローバルオブジェクトが取れる。
